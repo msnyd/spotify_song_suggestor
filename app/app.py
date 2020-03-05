7354e31ff2ccd985e3ad80a4ab1ebc0d89372ed6
msnyd <46577534+msnyd@users.noreply.github.com>
2020-03-05 15:31:55 -0600

    housekeeping
    doc strings, house keeping and update to .gitignore

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+# imported libraries
 from flask import Flask, jsonify
 import sqlite3
 import pandas as pd
@@ -10,13 +11,10 @@ from sklearn.model_selection import train_test_split
 from typing import List, Tuple
 
 DB = SQLAlchemy()
-#df = pd.read_csv(
-    #'https://raw.githubusercontent.com/msnyd/spotify_song_suggestor/master/most_popular_spotify_songs.csv')
 
-
+#Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
 class Songs(DB.Model):
     __tablename__ = "Songs"
-    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
     id = DB.Column(DB.BigInteger, primary_key=True)
     genre = DB.Column(DB.String(50))
     artist_name = DB.Column(DB.String(50))
@@ -41,20 +39,18 @@ class Songs(DB.Model):
         return '<Song {}>'.format(self.track_name)
 
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
+'''
+    This calls to the __init__ function to initialize the flask app. Upon instantiation, 
+    the app populates the database and runs the nearest neighbors Machine
+    Learning model.
+'''
 def create_app():
+
     app = Flask(__name__)
 
-    #DB = SQLAlchemy()
-
+    # Makes the database persist on Heroku
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://Spotify_Songs.db"
-    
+    # Connects and populates the database with the given structure of class Songs
     engine = create_engine('sqlite:///Spotify_Songs.db')
     Songs.metadata.create_all(engine)
     file_name = 'https://raw.githubusercontent.com/msnyd/spotify_song_suggestor/master/app/most_popular_spotify_songs.csv'
@@ -62,7 +58,11 @@ def create_app():
     db = df.to_sql(con=engine, index_label='id',
                    name=Songs.__tablename__, if_exists='replace')
 
-
+    '''    
+        This pre_process function takes in a pandas dataframe and runs it through 
+        encoding code to standardize certain features, it also onehotencodes the dataframe to
+        make it all numeric.
+    '''
     def pre_process(df):
         time_sig_encoding = {'0/4': 0, '1/4': 1,
                              '3/4': 3, '4/4': 4,
@@ -84,70 +84,55 @@ def create_app():
         df = encode_and_bind(df, 'genre')
         return df
 
-    """
-    This will run the nearest neighbors model when we instantiate the app.
-    """
+    '''
+        This will run the nearest neighbors model when we instantiate the app.
+    '''
     processed_df = pre_process(df)
 
     neigh = NearestNeighbors(n_neighbors=11)
     features = list(processed_df.columns[4:])
     X = processed_df[features].values
     neigh.fit(X)
-
+    '''
+        Takes in a dataframe, a feature set array and a song ID and 
+        returns a list of tuples of Artist Name and Track Name
+    '''
     def closest_ten(df: pd.DataFrame, X_array: np.ndarray, song_id: int) -> List[Tuple]:
         song = df.iloc[song_id]
         X_song = X[song_id]
         _, neighbors = neigh.kneighbors(np.array([X_song]))
         return neighbors[0][1:]
+    
+    # accepts the cursor and the row as a tuple and returns a dictionary result and you can object column by name 
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
 
-    # @app.route('/populate')
-    # def populate():
-    #     engine = create_engine('sqlite:///Spotify_Songs.db')
-    #     Songs.metadata.create_all(engine)
-    #     file_name = 'https://raw.githubusercontent.com/msnyd/spotify_song_suggestor/master/most_popular_spotify_songs.csv'
-    #     df = pd.read_csv(file_name)
-    #     db = df.to_sql(con=engine, index_label='id',
-    #             name=Songs.__tablename__, if_exists='replace')
-    #     return "Database has been made!"
-
+    # Landing Page
     @app.route('/')
     def hello_world():
 
-        return "Welcome to our Spotify API!"
+        return "Welcome to our Spotify Song Suggester API!"
 
-    # #TODO make a route that takes in json data and converts it to match the database?
-    # @app.route('/user/data')
-    # def user_data():
-    #     pass
-
-    # #Model returns a list of songs and we return the top 10
-    # @app.route('/songs', methods=['GET'])
-    # def get_songs():
-    #     conn = sqlite3.connect('sqlite://Spotify_Songs.db')
-    #     conn.row_factory = dict_factory
-    #     curs = conn.cursor()
-    #     all_songs = curs.execute(
-    #         'SELECT track_name, artist_name, genre FROM songs LIMIT 10;').fetchall()
-
-    #     return jsonify(all_songs)
-
-    @app.route('/track/<track_id>', methods=['GET'])  # /<track_id>
+    '''
+        Main API route that takes in a Track ID sent from the user and pairs it with
+        a unique track_id from the database and returns the closest songs from our
+        Machine Learning model.
+    '''
+    @app.route('/track/<track_id>', methods=['GET'])
     def track(track_id):
         track_id = int(track_id)
         conn = sqlite3.connect('Spotify_Songs.db')
         conn.row_factory = dict_factory
         curs = conn.cursor()
-        #idx = curs.execute(f'SELECT id from songs where track_id=={track_id};').fetchall()
         songlist = []
         song_recs = closest_ten(df, X, track_id)
         for idx in song_recs:
             song = curs.execute(
                 f'SELECT DISTINCT * FROM Songs WHERE id=={idx};').fetchone()
-
             songlist.append(song)
-        #songlist = tuple(songlist)
-        #songlist = list(dict.fromkeys(songlist)) # removes duplicates
-
         return jsonify(songlist)
 
     return app
